d53b7e9335abf764a1bb740a367182a56489a7df
Masha Gutkovskaya <gutkovskaya18@gmail.com>
2024-09-18 10:00:46 +0300

    add it to videos response

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,7 @@ class Utils {
     transformSearchResponse(items) {
         const results = items.map((item) => {
             return {
+                id : item.id.videoId,
                 title : item.snippet.title,
                 description : item.snippet.description,
                 picture :item.snippet.thumbnails.high.url
